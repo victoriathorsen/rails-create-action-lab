a3fdfc39aa8ba1cd54f525b60dac142028a56e6c
Victoria Thorsen <victoriathorsenn@gmail.com>
2021-02-23 17:47:34 -0500

    commit tree ee859f550e9226a9eee900c0d1142d95b621db75

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,9 +9,17 @@ class StudentsController < ApplicationController
   end
 
   def new
+
   end
 
   def create
+    @student = Student.create (
+      {
+        first_name: params[:first_name],
+        last_name: params[:last_name]
+      }
+    )
+    redirect_to student_path(@student)
   end
 
 end
